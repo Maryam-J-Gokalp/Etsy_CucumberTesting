a00bd196c39ba8d88f0a13d3cd097e73d2f5960c
Yasin <yasin.deger48@gmail.com>
2022-06-21 03:17:33 +0300

    First step definition class is created for the sign up

---
+++ src/test/java/com/etsy/step_definitions/SignUp_StepDef.java
@@ -0,0 +1,49 @@
+package com.etsy.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SignUp_StepDef {
+
+    @Given("user in on the etsy home page")
+    public void user_in_on_the_etsy_home_page() {
+
+
+    }
+    @When("user clicks the sign in button")
+    public void user_clicks_the_sign_in_button() {
+
+
+    }
+    @When("user enters the email address into the email box")
+    public void user_enters_the_email_address_into_the_email_box() {
+
+
+    }
+    @When("user click the continue button")
+    public void user_click_the_continue_button() {
+
+
+    }
+    @When("user enters the firstname into the firstname box")
+    public void user_enters_the_firstname_into_the_firstname_box() {
+
+
+    }
+    @When("user enters the password into the password box")
+    public void user_enters_the_password_into_the_password_box() {
+
+
+    }
+    @When("user clicks the register button")
+    public void user_clicks_the_register_button() {
+
+
+    }
+    @Then("user should see the etsy homepage")
+    public void user_should_see_the_etsy_homepage() {
+
+
+    }
+}
