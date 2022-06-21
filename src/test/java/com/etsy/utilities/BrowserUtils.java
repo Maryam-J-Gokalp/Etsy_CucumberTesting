9cacd40bc46375493f143e1bb625b6a2046ba1db
Yasin <yasin.deger48@gmail.com>
2022-06-21 03:00:22 +0300

    Static sleep methods is created in the BrowserUtils

--- src/test/java/com/etsy/utilities/BrowserUtils.java
+++ src/test/java/com/etsy/utilities/BrowserUtils.java
@@ -1,4 +1,17 @@
 package com.etsy.utilities;
 
 public class BrowserUtils {
+    /**
+     * This method only using for static waiting
+     * accept seconds
+     * @param seconds
+     */
+
+    public static void sleep(int seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
 }
